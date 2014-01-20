cd40ea22155293af8f57f7b527adbcff570d5ce9
Robert Pyke <robert.j.pyke@gmail.com>
2014-01-20 13:51:07 +1000

    Adds 'dummy' test. Adds travis support

--- setup.py
+++ setup.py
@@ -14,31 +14,43 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
+tests_require = [
+        'nose',
+        'coverage',
+        'mock',
+        'ipdb',
+]
+
 setup(name='requests_extensions.adapters',
-      version=version,
-      description="Adds dynamic loading and selection of adapters to requests",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+    version=version,
+    description="Adds dynamic loading and selection of adapters to requests",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Robert Pyke',
-      author_email='developer.robert.j.pyke@gmail.com',
-      url='https://github.com/robertpyke/requests_extensions.adapters',
-      license='MIT',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['requests_extensions'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'requests',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='',
+    author='Robert Pyke',
+    author_email='developer.robert.j.pyke@gmail.com',
+    url='https://github.com/robertpyke/requests_extensions.adapters',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['requests_extensions'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'requests',
+    ],
+    extras_require={
+        'test': tests_require,
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    test_suite = 'nose.collector',
+)
+
