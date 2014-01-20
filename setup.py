07148e61407a6952b17b547d1723a09cfd44d180
Robert Pyke <robert.j.pyke@gmail.com>
2014-01-20 12:15:09 +1000

    first commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='requests_extensions.adapters',
+      version=version,
+      description="Adds dynamic loading and selection of adapters to requests",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Robert Pyke',
+      author_email='developer.robert.j.pyke@gmail.com',
+      url='https://github.com/robertpyke/requests_extensions.adapters',
+      license='mit',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['requests_extensions'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
